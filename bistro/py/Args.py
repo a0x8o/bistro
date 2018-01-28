d60b4df05f5eac09890addabe3729947c6de8753
Alex B <axbaretto@gmail.com>
2018-01-28 08:14:04 -0800

    Updating submodules Suppress lint in old files Auto-fix add BSD headers in Python files Auto-fix BSD headers

--- bistro/py/Args.py
+++ bistro/py/Args.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -22,7 +29,7 @@ class Args(object):
     @classmethod
     def enrichParser(cls, parser):
         cls.PARSERS.append(parser)
-        for func in cls.ACTIONS.itervalues():
+        for func in cls.ACTIONS.itervalues():  # noqa: B301 T25377293 Grandfathered in
             func(parser)
         return parser
 
