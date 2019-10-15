fc6b2c0fec1c65683ce108d509bdc3d324d4ea10
Alex Barreto <axbarreto@lavabit.com>
2019-10-15 12:08:51 -0700

    Updating submodules
    Summary:
    GitHub commits:
    
    https://github.com/facebook/folly/commit/b797de382febdc9696bc2a38a388e1c9de0be053
    https://github.com/facebook/wangle/commit/7b96a55f8d48e53a963b90646a5d73390801a1ea
    https://github.com/facebookincubator/katran/commit/d96c44f35ad63e10688a879156fdee0d39b87a82
    
    Reviewed By: yns88
    
    fbshipit-source-id: 8bdb23e050087cef10007e66b5d896262ea615e0

--- build/fbcode_builder/getdeps/builder.py
+++ build/fbcode_builder/getdeps/builder.py
@@ -1,9 +1,7 @@
-# Copyright (c) 2019-present, Facebook, Inc.
-# All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
@@ -504,6 +502,7 @@ if __name__ == "__main__":
                 self.build_opts.fbsource_dir,
                 "--buck-test-info",
                 buck_test_info_name,
+                "--retry=3",
                 "--test-config",
                 "platform=%s" % machine_suffix,
                 "buildsystem=getdeps",
